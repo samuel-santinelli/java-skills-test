5f13686796c1b0c4aa6cfa65eb614e59b2e793b6
samuel-santinelli <samuelsantinelli@gmail.com>
2023-07-27 16:47:09 -0300

    feature: Importing the new Date method

--- src/fundamentals/Import.java
+++ src/fundamentals/Import.java
@@ -1,7 +1,13 @@
 package fundamentals;
 
+import java.util.Date;
+
 public class Import {
     public static void main(String[] args) {
-        
+        String s = "Bom dia!";
+        System.out.println(s);
+
+        Date d = new Date();
+        System.out.println(d);
     }
 }
