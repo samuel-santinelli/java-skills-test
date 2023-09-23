dcbbf009d3a1535baf6805d4940598bad411ba83
samuel-santinelli <samuelsantinelli@gmail.com>
2023-09-23 12:00:51 -0300

    feature: Changing the main method in PrimitivoVsObjeto class

--- src/fundamentals/PrimitivoVsObjeto.java
+++ src/fundamentals/PrimitivoVsObjeto.java
@@ -2,6 +2,11 @@ package fundamentals;
 
 public class PrimitivoVsObjeto {
     public static void main(String[] args) {
-        
+        String s = new String("text");
+        s.toUpperCase();
+
+        // Wrapper são a versão objeto dos tipos primitivos
+        int a = 123;
+        System.out.println(a);
     }
 }
