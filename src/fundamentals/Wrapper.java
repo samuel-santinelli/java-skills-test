540e34e01b40eaf9dde31ca9f2bb697ec6a15c5c
samuel-santinelli <samuelsantinelli@gmail.com>
2023-11-04 12:14:17 -0300

    feature: Converting String to Boolean in Wrapper class

--- src/fundamentals/Wrapper.java
+++ src/fundamentals/Wrapper.java
@@ -1,8 +1,24 @@
 package fundamentals;
 
+
 public class Wrapper {
-    Byte b = 100;
-    Short s = 1000;
-    Integer i = 10000;
-    Long l = 1000000L;
+    public static void main(String[] args) {
+
+
+        Byte b = 100;
+        Short s = 1000;
+        Integer i = 1000;
+        Long l = 1000000L;
+        Double r = 100000D;
+
+        System.out.println(b.byteValue());
+        System.out.println(s.toString());
+        System.out.println(i * 3);
+        System.out.println(l / 3);
+
+        Boolean bo = Boolean.parseBoolean("true");
+        System.out.println(bo);
+    }
+
+
 }
