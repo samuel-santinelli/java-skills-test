2bdcbcf31a9475d02be1f392dfc0e796cbdbe685
samuel-santinelli <samuelsantinelli@gmail.com>
2023-11-18 12:21:56 -0300

    feature: Adiing Boolean, Float and Double in Wrapper class

--- src/fundamentals/Wrapper.java
+++ src/fundamentals/Wrapper.java
@@ -16,8 +16,18 @@ public class Wrapper {
         System.out.println(i * 3);
         System.out.println(l / 3);
 
+        Float f = 123.10F;
+        System.out.println(f);
+
+        Double d = 1234.5678;
+        System.out.println(d);
+
         Boolean bo = Boolean.parseBoolean("true");
         System.out.println(bo);
+        System.out.println(bo.toString().toUpperCase());
+
+        Character c = '#';
+        System.out.println(c + "...");
     }
 
 
