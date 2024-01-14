1c519415c06372b483f7777896f96d58d953a7af
samuel-santinelli <samuelsantinelli@gmail.com>
2024-01-14 14:39:29 -0300

    feature: Creating the ConversaoStringNumero Class

--- src/fundamentals/ConversaoStringNumero.java
+++ src/fundamentals/ConversaoStringNumero.java
@@ -1,2 +1,18 @@
-package fundamentals;public class ConversaoStringNumero {
+package fundamentals;
+
+import javax.swing.*;
+
+public class ConversaoStringNumero {
+    public static void main(String[] args) {
+        String value1 = JOptionPane.showInputDialog("Write the first number, please: ");
+        String value2 = JOptionPane.showInputDialog("Write the second number, please: ");
+
+        System.out.println(value1 + value2);
+
+        double number1 = Double.parseDouble(value1);
+        double number2 = Double.parseDouble(value2);
+
+        double sum = number1 + number2;
+        System.out.println("Sum: " + sum);
+    }
 }
