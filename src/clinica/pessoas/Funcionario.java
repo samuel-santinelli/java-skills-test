278ddc80114f0d080329b679448b81aa66bcfd91
samuel-santinelli <samuelsantinelli@gmail.com>
2024-02-23 15:58:58 -0300

    feature: Formating the Funcionario class

--- src/clinica/pessoas/Funcionario.java
+++ src/clinica/pessoas/Funcionario.java
@@ -1,4 +1,5 @@
 package clinica.pessoas;
+
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.Arrays;
@@ -6,12 +7,10 @@ import java.util.List;
 import java.util.Random;
 
 public class Funcionario {
-
     private static final Logger logger = Logger.getLogger(Funcionario.class.getName());
     private static final List<String> names = Arrays.asList(
             "Alice", "Bob", "Charlie", "Diana", "Edward",
-            "Fiona", "George", "Hannah", "Ivan", "Jasmine"
-    );
+            "Fiona", "George", "Hannah", "Ivan", "Jasmine");
 
     public static void main(String[] args) {
         Funcionario funcionario = new Funcionario();
@@ -23,7 +22,7 @@ public class Funcionario {
 
         for (int i = 0; i < numberOfNames; i++) {
             String randomName = names.get(random.nextInt(names.size()));
-            logger.log(Level.INFO, "Random name {0}: {1}", new Object[]{i + 1, randomName});
+            logger.log(Level.INFO, "Random name {0}: {1}", new Object[] { i + 1, randomName });
         }
     }
 }
