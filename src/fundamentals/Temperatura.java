60b6488c8cf3e16fe976f7ff587e5ce03d7b86ff
samuel-santinelli <samuelsantinelli@gmail.com>
2024-03-01 16:38:03 -0300

    Changing the 'Temperatura' class

--- src/fundamentals/Temperatura.java
+++ src/fundamentals/Temperatura.java
@@ -1,2 +1,17 @@
-package fundamentals;public class Temperatura {
+package fundamentals;
+
+public class Temperatura {
+    public static void main(String[] args) {
+        final double FATOR = 5.0 / 9.0;
+        final double AJUSTE = 32;
+
+        double fahrenheit = 86;
+        double celsius = (fahrenheit - AJUSTE) * FATOR;
+        System.out.println("O resultado é: " + celsius + "ºC.");
+
+        fahrenheit = 0;
+        celsius = (fahrenheit - AJUSTE) * FATOR;
+        System.out.println("O resultado é: " + celsius + "ºC.");
+
+    }
 }
