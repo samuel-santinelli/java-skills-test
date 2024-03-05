21d308320873309803e10d9e59878814008d5cd7
samuel-santinelli <samuelsantinelli@gmail.com>
2024-03-05 15:59:24 -0300

    feature: Creating variables in TiposPrimitivos class

--- src/fundamentals/TiposPrimitivos.java
+++ src/fundamentals/TiposPrimitivos.java
@@ -1,4 +1,9 @@
 package fundamentals;
 
 public class TiposPrimitivos {
+    public static void main(String[] args) {
+        byte anosDeEmpresa = 23;
+        short numeroDeVoos = 32767;
+        int id = 56789;
+    }
 }
