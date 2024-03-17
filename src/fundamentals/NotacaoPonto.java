4f0ae393056fa02e629fcddb6de1ea499a10a0a4
samuel-santinelli <samuelsantinelli@gmail.com>
2024-03-17 16:33:44 -0300

    feature: Impementing the toUpperCase in enviroments

--- src/fundamentals/NotacaoPonto.java
+++ src/fundamentals/NotacaoPonto.java
@@ -2,7 +2,8 @@ package fundamentals;
 
 public class NotacaoPonto {
     public static void main(String[] args) {
-        double a = 2.3;
         String s = "Bom dia X";
+        s = s.toUpperCase();
+        System.out.println(s);
     }
 }
