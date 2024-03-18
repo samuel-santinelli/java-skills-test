f0d664989c5985686e6bfc0f34e4750378470f51
samuel-santinelli <samuelsantinelli@gmail.com>
2024-03-18 16:34:57 -0300

    feature: Impementing the replace method in enviroment

--- src/fundamentals/NotacaoPonto.java
+++ src/fundamentals/NotacaoPonto.java
@@ -4,6 +4,7 @@ public class NotacaoPonto {
     public static void main(String[] args) {
         String s = "Bom dia X";
         s = s.toUpperCase();
+        s = s.replaceAll("X", "Senhora");
         System.out.println(s);
     }
 }
