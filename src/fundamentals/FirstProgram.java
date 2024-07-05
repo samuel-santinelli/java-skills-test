229b22b373777fcab6836616557e540a54325755
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-05 15:49:42 -0300

    feature: Files and folders created, and java installed

--- src/fundamentals/FirstProgram.java
+++ src/fundamentals/FirstProgram.java
@@ -2,7 +2,6 @@ package fundamentals;
 
 public class FirstProgram {
     public static void main(String[] args) {
-        System.out.println("Hello word!");
-        System.out.println("Hello word!");
+        System.out.println("Hello word!");        
     }
 }
