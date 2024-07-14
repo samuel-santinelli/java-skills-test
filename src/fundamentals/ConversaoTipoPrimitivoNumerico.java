7f2eaead61431aced933a56b8077dd4f04d71b88
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-14 16:36:56 -0300

    feature: Creating the explicit and implicit conversion (CAST)

--- src/fundamentals/ConversaoTipoPrimitivoNumerico.java
+++ src/fundamentals/ConversaoTipoPrimitivoNumerico.java
@@ -1,5 +1,21 @@
 package fundamentals;
 
 public class ConversaoTipoPrimitivoNumerico {
-    double a = 1;
+    public static void main(String[] args) {
+        // Implicit conversion
+        double a = 1;
+        System.out.println(a);
+
+        // Explicit conversion (CAST)
+        float b = (float) 1.12345;
+        System.out.println(b);
+
+        int c = 340;
+        byte d = (byte) c;
+        System.out.println(d);
+
+        double e = 1.99999999;
+        int f = (int) e;
+        System.out.println(f);
+    }
 }
