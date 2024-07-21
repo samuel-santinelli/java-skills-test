ad5443a0462e1b012471ff15baf170f8fc27bb77
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-21 12:10:32 -0300

    feature: Testing the Relational Class and creating implementings

--- src/fundamentals/operadores/Relacionais.java
+++ src/fundamentals/operadores/Relacionais.java
@@ -1,4 +1,25 @@
 package fundamentals.operadores;
 
 public class Relacionais {
+    public static void main(String[] args) {
+        int a = 97;
+        int b = 'a';
+
+
+        System.out.println(a == b);
+
+        System.out.println(3 > 4);
+        System.out.println(3 >= 3);
+        System.out.println(3 < 7);
+        System.out.println(30 <= 7);
+        System.out.println(30 != 7);
+
+        double note = 9.9;
+        boolean goodBehavior = false;
+        boolean passByAverage = note >= 7;
+        boolean hasDiscount = goodBehavior && passByAverage;
+
+
+        System.out.println("Has discounts?" + hasDiscount);
+    }
 }
