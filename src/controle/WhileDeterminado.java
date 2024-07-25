a9eaaafb7918aad0649ae45652389aeef5f816de
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-25 08:43:51 -0300

    feature: Sending the while determinated

---
+++ src/controle/WhileDeterminado.java
@@ -0,0 +1,11 @@
+package controle;
+
+public class WhileDeterminado {
+    public static void main(String[] args) {
+        int count = 1;
+        while(count <= 10) {
+            System.out.println("Good morning!");
+            count++;
+        }
+    }
+}
