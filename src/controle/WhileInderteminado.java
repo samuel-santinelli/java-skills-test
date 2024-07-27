125a78daa1cc1b546ee0f81935d078a8f6e6207a
samuel-santinelli <samuelsantinelli@gmail.com>
2024-07-27 10:34:00 -0300

    feat: Creating the indertemined while Class

--- src/controle/WhileInderteminado.java
+++ src/controle/WhileInderteminado.java
@@ -1,2 +1,17 @@
-package controle;public class WhileInderteminado {
+package controle;
+
+import java.util.Scanner;
+
+public class WhileInderteminado {
+    public static void main(String[] args) {
+        Scanner entry = new Scanner(System.in);
+        String value = "";
+
+        while (!value.equalsIgnoreCase("exit")) {
+            System.out.println("You say: ");
+            entry.nextLine();
+        }
+
+        entry.close();
+    }
 }
