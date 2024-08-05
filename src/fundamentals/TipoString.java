f9377acd0890288b9834728d8d9917e17af89ad7
samuel-santinelli <samuelsantinelli@gmail.com>
2024-08-05 16:55:41 -0300

    fix: Delting lines

--- src/fundamentals/TipoString.java
+++ src/fundamentals/TipoString.java
@@ -3,7 +3,6 @@ package fundamentals;
 public class TipoString {
     public static void main(String[] args) {
         System.out.println("Olá pessoal".charAt(2));
-
         String s = "Boa tarde";
         System.out.println(s.concat("!!!"));
         System.out.println(s + "!!!");
