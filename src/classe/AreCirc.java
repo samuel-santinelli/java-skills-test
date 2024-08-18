83b8b167a2c19b5a853078d9517688de0b322275
samuel-santinelli <samuelsantinelli@gmail.com>
2024-08-18 16:39:21 -0300

    feature(DataTeste) - Fix the data teste constructor

--- src/classe/AreCirc.java
+++ src/classe/AreCirc.java
@@ -1,4 +1,16 @@
 package classe;
 
 public class AreCirc {
+
+    double raio;
+    double pi;
+
+    AreaCirc(double raioInicial){
+        pi = 3.14;
+        raio = raioInicial;
+    }
+   double area(){
+        return pi * Math.pow(raio, 2);
+   }
+
 }
