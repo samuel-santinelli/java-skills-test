2e6b76a9ab75ae0a4ca65bba16553a3252a846be
samuel-santinelli <samuelsantinelli@gmail.com>
2024-08-21 10:06:41 -0300

    feature(AreaCirc) - Creating the AreaCirc Class

--- src/classe/AreaCirc.java
+++ src/classe/AreaCirc.java
@@ -3,14 +3,18 @@ package classe;
 public class AreaCirc {
 
     double raio;
-    static double pi;
+    final static double PI = 3.1415;
 
     AreaCirc(double raioInicial){
-        pi = 3.14;
         raio = raioInicial;
     }
+
    double area(){
-        return pi * Math.pow(raio, 2);
+        return PI * Math.pow(raio, 2);
+   }
+
+   static double area(double raio){
+       return PI * Math.pow(raio, 2);
    }
 
 }
