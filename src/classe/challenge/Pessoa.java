f65b659c63ca65a85ec360acc5b99097ebcbce1d
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-02 14:36:23 -0300

    feature(Pessoa) - Creating the Pessoa class

--- src/classe/challenge/Pessoa.java
+++ src/classe/challenge/Pessoa.java
@@ -1,4 +1,17 @@
 package classe.challenge;
 
 public class Pessoa {
+    String name;
+    double weight;
+
+    Pessoa(String name, double weight){
+        this.name = name;
+        this.weight = weight;
+    }
+
+    void eat(Comida food){
+        if(food != null){
+            this.weight += food.weight;
+        }
+    }
 }
