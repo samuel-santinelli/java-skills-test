6f7fa2f06aacee323bdb11fb9f9bbfb23de8f920
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-04 14:44:58 -0300

    feature(Jantar) - Implementing the methods in Jantar.class

--- src/classe/challenge/Jantar.java
+++ src/classe/challenge/Jantar.java
@@ -1,4 +1,17 @@
 package classe.challenge;
 
 public class Jantar {
+    public static void main(String[] args) {
+        Comida c1 = new Comida("Rice", 0.223);
+        Comida c2 = new Comida("Beans", 0.300);
+
+        Pessoa p = new Pessoa("John", 99.8);
+
+        System.out.println(p.present());
+        p.eat(c1);
+        System.out.println(p.present());
+        p.eat(c2);
+
+        System.out.println(p.present());
+    }
 }
