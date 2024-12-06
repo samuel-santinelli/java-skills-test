49465bf0c16211e1a611b05f171cd6ec60068c10
samuel-santinelli <samuelsantinelli@gmail.com>
2024-12-06 14:15:53 -0300

    feature(Vectors) - Creating the Exercicio Class

--- src/arrays/Exercicio.java
+++ src/arrays/Exercicio.java
@@ -1,4 +1,24 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class Exercicio {
+    public static void main(String[] args) {
+
+        double[] notesStudentA = new double[3];
+        System.out.println(Arrays.toString(notesStudentA));
+
+        notesStudentA[0] = 7.9;
+        notesStudentA[1] = 8;
+        notesStudentA[2] = 6.7;
+
+        System.out.println(Arrays.toString(notesStudentA));
+
+        double total = 0;
+        for (int i = 0; i < 3; i++) {
+            total += notesStudentA[i];
+        }
+
+        System.out.println(total / 3);
+    }
 }
